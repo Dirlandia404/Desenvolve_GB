ce49a02f4b3b12ce139f4b15bf9a9aa1afdc266c
Dirlandia404 <dirlandiaoliveira@alu.ufc.br>
2024-06-11 14:16:02 -0300

    projeto finalizado

---
+++ monibank-main/js/valida-cpf.js
@@ -0,0 +1,56 @@
+export default function ehUmCPF(campo) {
+  const cpf = campo.value.replace(/\.|-/g, "");
+  if (validaRepetidos(cpf) || validaPrimeiro(cpf) || validaSegundo(cpf)) {
+    campo.setCustomValidity("esse cpf não é valido");
+  }
+}
+function validaRepetidos(cpf) {
+  const numeros = [
+    "00000000000",
+    "11111111111",
+    "22222222222",
+    "33333333333",
+    "44444444444",
+    "55555555555",
+    "66666666666",
+    "77777777777",
+    "88888888888",
+    "99999999999",
+  ];
+  return numeros.includes(cpf);
+}
+
+function validaPrimeiro(cpf) {
+  let soma = 0;
+
+  let multiplicador = 10;
+
+  for (let tamanho = 0; tamanho < 9; tamanho++) {
+    soma += cpf[tamanho] * multiplicador;
+    multiplicador--;
+  }
+
+  soma = (soma * 10) % 11;
+
+  if (soma == 10 || soma == 11) {
+    soma = 0;
+  }
+  return soma != cpf[9];
+}
+function validaSegundo(cpf) {
+  let soma = 0;
+
+  let multiplicador = 11;
+
+  for (let tamanho = 0; tamanho < 10; tamanho++) {
+    soma += cpf[tamanho] * multiplicador;
+    multiplicador--;
+  }
+
+  soma = (soma * 10) % 11;
+
+  if (soma == 10 || soma == 11) {
+    soma = 0;
+  }
+  return soma != cpf[10];
+}
